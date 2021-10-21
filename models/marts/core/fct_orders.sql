1e9fc885bd6fa55c9e2ae8de8e53422171724c1f
Baurzhan Kuspayev <kuspayev@aifund.kz>
2021-10-21 11:56:03 +0000

    enriched jaffle_shop table with ltv column

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,16 +1,30 @@
-with payments as (
-    select * from {{ ref('stg_payments')}}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
-orders as (
-    select * from {{ ref('stg_orders')}}
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select
-    ods.order_id,
-    ods.customer_id,
-    amount
-from
-    orders ods 
-    left join 
-    payments pts on ods.order_id = pts.order_id
+select * from final
